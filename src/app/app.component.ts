e305fb6b632ed33516cc31b425bb7190ed428eac
Wiame Elalami <wiameelalami02@gmail.com>
2024-01-22 12:00:58 +0100

    maj amélioration et structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
 import { DatePipe } from '@angular/common'
 import { AproposComponent } from "./apropos/apropos.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, UpperCasePipe, DatePipe, AproposComponent, WelcomeComponent, RouterLink]
+    imports: [UpperCasePipe, DatePipe, AproposComponent, WelcomeComponent, RouterLink, RouterOutlet]
 })
 
 export class AppComponent {
