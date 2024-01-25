3bf95ebbdb768d82d6f867ecb89a614d88bf5913
Wiame Elalami <wiameelalami02@gmail.com>
2024-01-25 12:06:14 +0100

    Resolution de problemes et finitions

--- src/app/apropos/equipe/equipe.component.ts
+++ src/app/apropos/equipe/equipe.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-equipe',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './equipe.component.html',
   styleUrl: './equipe.component.css'
 })
